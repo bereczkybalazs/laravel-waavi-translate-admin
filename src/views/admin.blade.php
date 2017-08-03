e7f2f88114ac4e4d50ad9b5520bd2ded5fbbb083
Bereczky Balázs <bereczkybalazs1@gmail.com>
2017-08-03 15:18:41 +0200

    added base layout

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -1,45 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel Timezones</title>
+@extends('vendor.translate_admin.layout.app')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('translate_admin')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <h1 class="title">{{$test}}</h1>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">{{$test}}</div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
