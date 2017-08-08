fc428e73287d296b1ae7f4766043b071c23a5e53
Bereczky Balázs <bereczkybalazs1@gmail.com>
2017-08-08 12:11:55 +0200

    data reading

--- src/resources/translate-admin-assets/js/index.js
+++ src/resources/translate-admin-assets/js/index.js
@@ -1,1 +1,44 @@
-console.log('test');
+var translate = function () {
+    var translate = {};
+    this.initDom();
+}
+
+translate.prototype.initDom = function () {
+    var that = this;
+    $('.translation-group').click(function(){
+        $('.translation-group').removeClass('active');
+        $(this).addClass('active');
+        that.translate = JSON.parse($(this).attr('data-translate'));
+        that.renderInputs();
+    });
+    $('.language-switch-input').on('change', function () {
+        that.renderInputs();
+    })
+}
+
+translate.prototype.renderInputs = function () {
+    this.renderById('translateLanguageOne');
+    this.renderById('translateLanguageTwo');
+}
+
+translate.prototype.renderById = function (id) {
+    var domElement = $('#' + id);
+    var locale = domElement.val();
+    var listId = domElement.attr('data-render-list');
+    $('#' + listId).html('');
+    for (var i in this.translate[locale]) {
+        var item = this.translate[locale][i];
+        this.addInput(listId, item.id, item.text)
+    }
+}
+
+translate.prototype.addInput = function (listId, name, value) {
+
+    $('#' + listId).append(
+        '<div class="translate-input form-group">' +
+            '<input type="text" class="form-control" name="' + name + '" value="' + value + '">' +
+        '</div>'
+    );
+}
+
+new translate();
--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -14,23 +14,24 @@
                     <h2>Translation groups</h2>
                 </div>
                 <div class="translation-groups scroll row border-bottom-label">
-                    <div class="translation-group border-bottom-label">
-                        group1
-                    </div>
-                    <div class="translation-group active border-bottom-label">
-                        group2
-                    </div>
+                    @foreach($data->translates->groups as $group)
+                        <div class="translation-group border-bottom-label" data-translate="{{json_encode($group->data)}}">
+                            {{$group->name}}
+                        </div>
+                    @endforeach
                 </div>
             </div>
             <div class="col-md-4 separated-label">
                 <div class="table-header-label border-bottom-label">
                     <h2>Select language</h2>
-                    <select name="languageOne" class="form-control" id="">
-                        <option>en</option>
-                        <option>de</option>
+                    <select name="languageOne" class="form-control language-switch-input" id="translateLanguageOne"
+                            data-render-list="translateListOne">
+                        @foreach($data->locales as $locale)
+                            <option value="{{$locale->locale}}">{{$locale->name}}</option>
+                        @endforeach
                     </select>
                 </div>
-                <div class="translate-inputs scroll row table-content-label border-bottom-label">
+                <div class="translate-inputs scroll row table-content-label border-bottom-label" id="translateListOne">
                     <div class="translate-input form-group">
                         <input type="text" class="form-control">
                     </div>
@@ -42,12 +43,13 @@
             <div class="col-md-4">
                 <div class="table-header-label border-bottom-label">
                     <h2>Select language</h2>
-                    <select name="languageTwo" class="form-control" id="">
-                        <option>en</option>
-                        <option>de</option>
+                    <select name="languageTwo" class="form-control language-switch-input" id="translateLanguageTwo" data-render-list="translateListTwo">
+                        @foreach($data->locales as $locale)
+                            <option value="{{$locale->locale}}">{{$locale->name}}</option>
+                        @endforeach
                     </select>
                 </div>
-                <div class="translate-inputs scroll row table-content-label border-bottom-label">
+                <div class="translate-inputs scroll row table-content-label border-bottom-label" id="translateListTwo">
                     <div class="translate-input form-group">
                         <input type="text" class="form-control">
                     </div>
